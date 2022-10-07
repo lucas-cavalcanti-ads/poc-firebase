98eba8248daafe8dc70a2406d40170bf9ec5269f
lucas-cavalcanti-ads <lucas.tnv27@gmail.com>
2022-10-06 23:26:17 -0300

    feat: criacao projeto e primeiro teste

---
+++ teste/apps/poc-firebase/src/app/nx-welcome.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+/* eslint-disable */
+
+@Component({
+  selector: 'teste-nx-welcome',
+  template: './nx-welcome.component.html',
+  styles: []  
+})
+export class NxWelcomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
